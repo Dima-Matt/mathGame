27afd2cf9764e465b5812687fd984ab08a21cc0f
Dima Mironov <mattmars9548@gmail.com>
2024-08-17 13:54:43 +0300

    hhh

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 
 
-const drop = document.querySelector('.drop');
 const screen = document.querySelector('.main__screen');
 const num1 = document.getElementById('num-1');
 const num2 = document.getElementById('num-2');
@@ -11,8 +10,29 @@ screen.addEventListener('click', createRandomDrope);
 
 function createRandomDrope() {
 
-       drop.classList.remove('hide')
-       drop.style.left = getRandomInt(1, 500) + 'px';        
+       // setInterval(function() {
+       //     const drop = document.createElement('div');
+       //     const inDrop = document.createElement('div');
+       //     drop.classList.add('drop');
+       //     inDrop.classList.add('drop-container');
+       //     drop.style.left = getRandomInt(1, 500) + 'px';
+       //     drop.append(inDrop);
+       //     screen.append(drop); 
+           
+       // }, 2000)
+
+       let x = `
+           <div class="drop hide">
+      <div class="drop-container">
+       <span class="drop__num-1" id="num-1">x</span>
+       <span class="drop__sign" id="sign">+</span>
+       <span class="drop__num-2" id="num-2">y</span>
+      </div>
+    </div>
+       `;
+
+       screen.insertAdjacentHTML('beforeBegin', x);
+            
 }
 
 
